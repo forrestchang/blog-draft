32f722cd9993766e9e4cac529b0d309d3a19b974
Jiayuan Zhang <forrestchang7@gmail.com>
2015-12-12 15:37:43 +0800

    autoupdate by script

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
+cd ~/org/blog/
+git add .
+git commit -m 'autoupdate by script'
+proxychains4 git push
+
+cd ~/org/public_html/
 git add .
 git commit -m 'autoupdate by script'
 proxychains4 git push
